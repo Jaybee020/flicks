53ee5fe4866774a7577841eda87ca35d2944de9b
codergon <atakerewilliams@gmail.com>
2023-10-01 19:57:34 +0100

    Fixing render deploy

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import { config } from "dotenv";
 config();
 export const NODE_ENV = process.env.NODE_ENV;
 const REDIS_URL = process.env.REDIS_URL;
-export let client = createClient();
+export let client = createClient({ url: REDIS_URL });
 client
   .on("error", function (error) {
     console.error(error);
